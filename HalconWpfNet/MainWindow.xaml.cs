dd30d531bc9c33b54aff0d136247bd79d2ecd201
Bob Voigt <rtibobv@outlook.com>
2017-07-20 07:49:27 -0400

    CodeMaid

--- HalconWpfNet/MainWindow.xaml.cs
+++ HalconWpfNet/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using HalconDotNet;
+using Microsoft.Win32;
 
 namespace HalconWpfNet
 {
@@ -20,9 +10,45 @@ namespace HalconWpfNet
      /// </summary>
      public partial class MainWindow : Window
      {
+          #region Public Constructors
+
           public MainWindow()
           {
                InitializeComponent();
           }
+
+          #endregion Public Constructors
+
+          #region Private Methods
+
+          private void ButtonReadImage_Click(object sender, RoutedEventArgs e)
+          {
+               var openFileDialog = new OpenFileDialog()
+               {
+                    InitialDirectory = Environment.GetEnvironmentVariable(@"HALCONEXAMPLES") + @"\images",
+                    Filter = "Image Files | *.png;*.jpg;*.jp2;*.tif;*.bmp;*.hobj;*.ima; |All Files | *.*"
+               };
+               if (openFileDialog.ShowDialog() == true)
+               {
+                    try
+                    {
+                         var image = new HImage(openFileDialog.FileName);
+                         HalconSmartWindow.HalconWindow.DispImage(image.CopyObj(1, -1));
+                         HalconSmartWindow.SetFullImagePart(null);
+                    }
+                    catch (HalconException halconException)
+                    {
+                         StatusMessage.Text = halconException.Message;
+                    }
+                    HelpMessage.Text = "Right click and drag image to move.\r\nMouse wheel zooms.\r\nDouble click image to recenter and fit.";
+               }
+          }
+
+          private void HalconSmartWindow_Initialized(object sender, EventArgs e)
+          {
+               //(sender as HSmartWindowControlWPF).HKeepAspectRatio = true;
+          }
+
+          #endregion Private Methods
      }
 }
